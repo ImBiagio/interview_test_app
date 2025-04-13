f644ef7ad536518272afe50adfac69a07cd4a08b
ImBiagio <caracciolo.biagio1996@gmail.com>
2025-04-13 13:59:07 +0200

    Add list Article CBV example

--- blogapp/blog/urls.py
+++ blogapp/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import article_list
+from .views import article_list, ArticleListView
 
 urlpatterns = [
     path('list-fbv', article_list, name='article_list'),
+    path('list-cbv', ArticleListView.as_view(), name='article_list'),
+
 ]
--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
 from .models import Article
 
 
+# Qui ho scritto 2 diverse view che fanno la stessa cosa, la prima è una function based view (FBV) e la seconda è una class based view (CBV).
+# Class Based View (FBV)
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'blog/article_list.html'
+    context_object_name = 'articles'
+    ordering = ['-published_date']
+
+
+# Function Based View (CBV)
 def article_list(request):
     articles = Article.objects.all().order_by('-published_date')
     return render(request, 'blog/article_list.html', {'articles': articles})

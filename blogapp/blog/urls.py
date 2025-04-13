efdf187c84a86dfe88f54b551ee0e78c5824610b
ImBiagio <caracciolo.biagio1996@gmail.com>
2025-04-13 14:29:39 +0200

    Add CRUD Class Based Views

--- blogapp/blog/urls.py
+++ blogapp/blog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from .views import article_list, ArticleListView
+from .views import article_list, ArticleListView, ArticleDetailView, ArticleCreateView, ArticleUpdateView, \
+    ArticleDeleteView
 
 urlpatterns = [
-    path('list-fbv', article_list, name='article_list'),
+    path('list-fbv', article_list, name='article_list_fbv'),
     path('list-cbv', ArticleListView.as_view(), name='article_list'),
-
+    path('detail/<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
+    path('create/', ArticleCreateView.as_view(), name='article_create'),
+    path('update/<int:pk>/', ArticleUpdateView.as_view(), name='article_update'),
+    path('delete/<int:pk>/', ArticleDeleteView.as_view(), name='article_delete'),
 ]
--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from .models import Article
 
@@ -17,3 +18,37 @@ class ArticleListView(ListView):
 def article_list(request):
     articles = Article.objects.all().order_by('-published_date')
     return render(request, 'blog/article_list.html', {'articles': articles})
+
+
+# Detail view
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'blog/article_detail.html'
+    context_object_name = 'article'
+
+
+# Create view
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = 'blog/article_form.html'
+    fields = ['title', 'content']
+
+    def get_success_url(self):
+        return reverse_lazy('article_detail', kwargs={'pk': self.object.pk})
+
+
+# Update view
+class ArticleUpdateView(UpdateView):
+    model = Article
+    template_name = 'blog/article_form.html'
+    fields = ['title', 'content']
+
+    def get_success_url(self):
+        return reverse_lazy('article_detail', kwargs={'pk': self.object.pk})
+
+
+# Delete view
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = 'blog/article_delete.html'
+    success_url = reverse_lazy('article_list')
